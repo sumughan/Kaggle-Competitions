80da1141da59a23dd3d9798f079c426b30382ce2
Sumu <sumughan_a@yahoo.com>
2019-03-31 07:17:07 -0500

    First upload of 3 of my current Kaggle competition code

---
+++ Google Analytics Customer Revenue Prediction/GoogleAnalyticsRegression.py
@@ -0,0 +1,39 @@
+#first program for submission for google analytics competition.
+
+import os
+import json
+import numpy as np
+import pandas as pd
+from pandas.io.json import json_normalize
+import matplotlib.pyplot as plt
+import seaborn as sns
+color = sns.color_palette()
+
+
+def load_df(csv_path='train.csv', nrows=None):
+    JSON_COLUMNS = ['device', 'geoNetwork', 'totals', 'trafficSource']
+    
+    df = pd.read_csv(csv_path, 
+                     converters={column: json.loads for column in JSON_COLUMNS}, 
+                     dtype={'fullVisitorId': 'str'}, # Important!!
+                     nrows=nrows)
+    
+    for column in JSON_COLUMNS:
+        column_as_df = json_normalize(df[column])
+        column_as_df.columns = [f"{column}.{subcolumn}" for subcolumn in column_as_df.columns]
+        df = df.drop(column, axis=1).merge(column_as_df, right_index=True, left_index=True)
+    print(f"Loaded {os.path.basename(csv_path)}. Shape: {df.shape}")
+    return df
+
+train_df = load_df(nrows=10000)
+test_df = load_df(csv_path="test.csv",nrows=10000)
+
+
+train_df["totals.transactionRevenue"] = train_df["totals.transactionRevenue"].astype('float')
+gdf = train_df.groupby("fullVisitorId")["totals.transactionRevenue"].sum().reset_index()
+
+plt.figure(figsize=(8,6))
+plt.scatter(range(gdf.shape[0]), np.sort(np.log1p(gdf["totals.transactionRevenue"].values)))
+plt.xlabel('index', fontsize=12)
+plt.ylabel('TransactionRevenue', fontsize=12)
+plt.show()
